daa3af4cbef36b57d8dc82d0cd88eba2491604d5
pezholio <pezholio@gmail.com>
2014-01-13 13:29:24 +0000

    Add ErrorMessage class

--- lib/csvlint.rb
+++ lib/csvlint.rb
@@ -150,5 +150,17 @@ module Csvlint
     end
     
   end
+  
+  class ErrorMessage
+    
+    attr_reader :type, :row, :content
+    
+    def initialize(params)
+      params.each do |key, value|
+        self.instance_variable_set("@#{key}".to_sym, value)
+      end
+    end
+    
+  end
 
 end
