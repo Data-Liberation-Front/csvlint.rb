e9def6abffc7461a739cde27d050e890041b709f
Jez Higgins <jez@jezuk.co.uk>
2015-03-27 12:16:59 +0000

    Fixed warning - use expect( ) rather than .should

--- features/step_definitions/information_steps.rb
+++ features/step_definitions/information_steps.rb
@@ -4,10 +4,10 @@ end
 
 Then(/^the "(.*?)" should be "(.*?)"$/) do |type, encoding|
   validator = Csvlint::Validator.new( @url, default_csv_options ) 
-  validator.send(type.to_sym).should == encoding
+  expect( validator.send(type.to_sym) ).to eq( encoding )
 end
 
 Then(/^the metadata content type should be "(.*?)"$/) do |content_type|
   validator = Csvlint::Validator.new( @url, default_csv_options ) 
-  validator.headers['content-type'].should == content_type
+  expect( validator.headers['content-type'] ).to eq( content_type )
 end
