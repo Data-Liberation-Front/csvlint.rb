566f25aed83e722635f9877d99c22b1e2a330279
pezholio <pezholio@gmail.com>
2015-09-22 11:55:47 +0100

    Delete client and specs

--- lib/csvlint.rb
+++ lib/csvlint.rb
@@ -14,7 +14,6 @@ require 'csvlint/error_message'
 require 'csvlint/error_collector'
 require 'csvlint/validate'
 require 'csvlint/streaming_validate'
-require 'csvlint/streaming_client'
 require 'csvlint/wrapped_io'
 require 'csvlint/field'
 
