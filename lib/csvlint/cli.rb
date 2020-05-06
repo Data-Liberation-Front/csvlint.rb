81042be9ced5a1e739952fec2a7cacba23098177
Jez Higgins <jez@jezuk.co.uk>
2020-05-06 15:20:24 +0100

    Fix load_from_json deprecation warnings.
    Use Csvlint::Schema.load_from_uri instead, as directed by the warning.

--- lib/csvlint/cli.rb
+++ lib/csvlint/cli.rb
@@ -58,7 +58,7 @@ module Csvlint
 
       def get_schema(schema)
         begin
-          schema = Csvlint::Schema.load_from_json(schema, false)
+          schema = Csvlint::Schema.load_from_uri(schema, false)
         rescue Csvlint::Csvw::MetadataError => e
           return_error "invalid metadata: #{e.message}#{" at " + e.path if e.path}"
         rescue OpenURI::HTTPError, Errno::ENOENT
