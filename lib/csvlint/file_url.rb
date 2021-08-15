f931c7105fc0da00274593718ebc54ca348c3bc9
Stephen Thirlwall <stephent@strategicdata.com.au>
2021-08-16 09:27:21 +1000

    Apply URI encode/decode to path<->url conversions

--- lib/csvlint/file_url.rb
+++ lib/csvlint/file_url.rb
@@ -3,14 +3,15 @@ module Csvlint
 
     # Convert a path to an absolute file:// uri
     def FileUrl.url(path)
-      File.expand_path(path).gsub(/^\/*/, "file:///")
-      #URI.encode(File.expand_path(path).gsub(/^\/*/, "file:///"))
+      URI.encode(File.expand_path(path).gsub(/^\/*/, "file:///"))
     end
 
     # Convert an file:// uri to a File
     def FileUrl.file(uri)
       if uri.start_with?("file:")
-        File.new(uri.gsub(/^file:\/\//, ""))
+        uri = URI.decode(uri)
+        uri = uri.gsub(/^file:\/*/, "/")
+        File.new(uri)
       else
         uri
       end
